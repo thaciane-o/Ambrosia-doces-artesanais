5932fd7650e452597de96f1e524a143b18fa2657
thaciane-o <thacianeoliveira0207@gmail.com>
2023-03-08 20:47:51 -0300

    Página de contato terminada

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,35 @@
+import styles from "./Contact.module.css";
+import { FaEnvelope, FaInstagram, FaMapMarked, FaPhone, FaWhatsapp } from "react-icons/fa";
+
 function Contact() {
     return (
-        <h1>Contato</h1>
+        <div className={styles.container}>
+            <h1>Fale conosco</h1>
+            <ul>
+                <li>
+                    <h2><FaPhone /> Telefone</h2>
+                    <p>(37) 9 9935-8892</p>
+                </li>
+                <li>
+                    <h2><FaWhatsapp /> WhatsApp</h2>
+                    <p>wa.me/37999358892</p>
+                </li>
+                <li>
+                    <h2><FaInstagram /> Instagram</h2>
+                    <p>@ambrosia.doce</p>
+                </li>
+                <li>
+                    <h2><FaEnvelope /> Email</h2>
+                    <p>ambrosia.doce@hotmail.com</p>
+                </li>
+                <li>
+                    <h2><FaMapMarked /> Endereço</h2> 
+                    <p>Rua (nome da rua), 000
+                    <br/>
+                    (Nome do bairro) - Formiga, Minas Gerais</p>
+                </li>
+            </ul>
+        </div>
     );
 }
 
